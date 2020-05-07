8587d8fb7d775b21bba7947a014cb611cf18ad76
zhangxwww <631133544@qq.com>
2020-05-07 20:27:07 +0800

    edit user

--- backend/app/views/user.py
+++ backend/app/views/user.py
@@ -82,3 +82,43 @@ def login():
         'error_msg': ''
     }
 
+
+@user.route('/edit', methods=['POST'])
+def edit():
+    phone = session.get('phone')
+    if phone is None:
+        return {
+            'success': False,
+            'error_msg': 'Login please'
+        }
+    phone = int(phone)
+    u = User.query.filter(User.phone == phone).first()
+    password_old = request.form.get('password_old')
+    password_new = request.form.get('password_new')
+    if password_new is not None:
+        if password_old is None:
+            return {
+                'success': False,
+                'error_msg': 'Old password is required'
+        }
+        if password_old != u.password:
+            return {
+                'success': False,
+                'error_msg': 'Wrong password'
+            }
+        u.password = password_new
+
+    nickname = request.form.get('nickname')
+    if nickname is not None:
+        u.nickname = nickname
+
+    signature = request.form.get('signature')
+    if signature is not None:
+        u.signature = signature
+
+    db.session.commit()
+    return {
+        'success': True,
+        'error_msg': ''
+    }
+
