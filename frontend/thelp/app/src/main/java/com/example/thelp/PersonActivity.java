9a64b2ccdae711a9536e719020bc787926234069
agil27 <wang-yb16@mails.tsinghua.edu.cn>
2020-05-28 00:21:05 +0800

    finished personal information with dialogs and fixed bugs

--- frontend/thelp/app/src/main/java/com/example/thelp/PersonActivity.java
+++ frontend/thelp/app/src/main/java/com/example/thelp/PersonActivity.java
@@ -2,7 +2,13 @@ package com.example.thelp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 public class PersonActivity extends AppCompatActivity {
 
@@ -10,5 +16,49 @@ public class PersonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
+
+        TextView modifyPhoneButton = findViewById(R.id.modify_phone_button);
+        TextView modifyPasswordButton = findViewById(R.id.modify_password_button);
+        TextView modifySignatureButton = findViewById(R.id.modify_signature_button);
+        Context ctx = this;
+
+        modifyPhoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialAlertDialogBuilder(ctx)
+                        .setTitle("修改手机号")
+                        .setMessage("请输入修改后的内容并点击确定")
+                        .setView(R.layout.input_modify)
+                        .setNegativeButton("确定", null)
+                        .setPositiveButton("取消", null)
+                        .show();
+            }
+        });
+
+        modifySignatureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialAlertDialogBuilder(ctx)
+                        .setTitle("修改签名")
+                        .setMessage("请输入修改后的内容并点击确定")
+                        .setView(R.layout.input_modify)
+                        .setNegativeButton("确定", null)
+                        .setPositiveButton("取消", null)
+                        .show();
+            }
+        });
+
+        modifyPasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialAlertDialogBuilder(ctx)
+                        .setTitle("修改密码")
+                        .setMessage("请输入修改后的内容并点击确定")
+                        .setView(R.layout.input_modify)
+                        .setNegativeButton("确定", null)
+                        .setPositiveButton("取消", null)
+                        .show();
+            }
+        });
     }
 }
